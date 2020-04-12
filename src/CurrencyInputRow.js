abca2719a8e2c8417d478635f1260c1130cedc31
Croustys <51267148+Croustys@users.noreply.github.com>
2020-04-12 18:36:03 +0200

    final

--- src/CurrencyInputRow.js
+++ src/CurrencyInputRow.js
@@ -4,7 +4,7 @@ export default function CurrencyInputRow(prop) {
     const { options, selectCurrency, onChangeCurrency, onChangeAmount, amount } = prop
     return (
         <div>
-            <input onChange= {onChangeAmount} value = {String(amount)} type="number"></input>
+            <input className="input" onChange= {onChangeAmount} value = {String(amount)} type="number"></input>
             <select value={selectCurrency} onChange={onChangeCurrency}>
                 {options.map(op => (
                     <option key={op} value={op}>{op}</option>
