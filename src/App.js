e8c051ac81f48142b95827727596e0166e650900
Croustys <akos.barabas2002@gmail.com>
2021-04-21 16:06:49 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CurrencyInputRow from "./CurrencyInputRow";
 import "./App.css";
 
 const APIKEY = process.env.API_KEY || "";
-const URL = `http://data.fixer.io/api/latest?access_key=${APIKEY}`;
+const URL = `http://api.exchangeratesapi.io/v1/latest?access_key=${APIKEY}`;
 
 function App() {
   const [options, setCOptions] = useState([]);
