fb7362771de34f994473c0cf6379beb1374289c7
Croustys <akos.barabas2002@gmail.com>
2021-04-21 14:55:52 +0200

    Updates

---
+++ precache-manifest.aab2d50be4baa5e3cb2233062fe83cc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26749045809dd89d0879643e398ba56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b2c04fce7c51979b220",
+    "url": "/static/css/main.3fd5cdea.chunk.css"
+  },
+  {
+    "revision": "cd84d894bb569f407b6e",
+    "url": "/static/js/2.7d781e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d781e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2c04fce7c51979b220",
+    "url": "/static/js/main.2f1e9536.chunk.js"
+  },
+  {
+    "revision": "1f2ba14172dc67d8e65a",
+    "url": "/static/js/runtime-main.9604db6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DailyCurrency/precache-manifest.1e510bdc10dde856de8913b5e60130f4.js"
+  "/precache-manifest.aab2d50be4baa5e3cb2233062fe83cc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DailyCurrency/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
