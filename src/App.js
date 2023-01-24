aa5dc229468904730d49e4c9c9b2c1121528f0c7
Croustys <akos.barabas2002@gmail.com>
2023-01-24 14:57:20 +0100

    new api key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./App.css";
 
 // https://free.currencyconverterapi.com/
-const APIKEY = "a5f22d512768cd217187"; //gyerünk lopd el
+const APIKEY = "7966af3e6f18f393d597";
 const URL = `https://free.currconv.com/api/v7/`;
 const URL_countries = `https://free.currconv.com/api/v7/currencies?apiKey=${APIKEY}`;
 
